68af9151a5ee08746e15be5fa86f7b1b5f029e55
fujioka8700 <fujioka8700@gmail.com>
2022-10-22 07:47:33 +0900

    [update] スクロール位置

--- js/sample.js
+++ js/sample.js
@@ -1,15 +1,3 @@
-const one = document.querySelector('#one');
-one.addEventListener('click', function (e) {
-    console.log('One handler', e.target);
-});
-
-const two = document.querySelector('#two');
-two.addEventListener('click', function (e) {
-    console.log('Two handler', e.target.id);
-});
-
-const three = document.querySelector('#three');
-three.addEventListener('click', function (e) {
-    e.stopPropagation();
-    console.log('Three handler', e.target.id);
-});
+window.addEventListener('scroll', function(){
+    console.log(this.window.pageYOffset)
+})
