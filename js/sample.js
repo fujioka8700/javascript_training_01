7f3f7b8da8d7d0dcd173486199df4b8753fc1415
fujioka8700 <fujioka8700@gmail.com>
2022-10-21 23:07:06 +0900

    [update] イベントリスナー確認中

--- js/sample.js
+++ js/sample.js
@@ -1,32 +1,13 @@
-function pagination(c, m) {
-    var current = c,
-        last = m,
-        delta = 2,
-        left = current - delta,
-        right = current + delta + 1,
-        range = [],
-        rangeWithDots = [],
-        l;
+const element = document.querySelector('.square');
 
-    for (let i = 1; i <= last; i++) {
-        if (i == 1 || i == last || i >= left && i < right) {
-            range.push(i);
-        }
-    }
+element.addEventListener('mouseover', function(event) {
+    this.classList.toggle('radius');
+});
 
-    for (let i of range) {
-        if (l) {
-            if (i - l === 2) {
-                rangeWithDots.push(l + 1);
-            } else if (i - l !== 1) {
-                rangeWithDots.push('...');
-            }
-        }
-        rangeWithDots.push(i);
-        l = i;
-    }
+element.addEventListener('mouseleave', function(event) {
+    this.classList.toggle('radius');
+});
 
-    return rangeWithDots;
-}
-
-console.log(pagination(17,20))
+element.addEventListener('click', event => {
+    console.log(this)
+});
