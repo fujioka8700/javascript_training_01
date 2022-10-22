c543292a4743ce840a7eb2e75a10b718a2432b8c
fujioka8700 <fujioka8700@gmail.com>
2022-10-22 16:32:08 +0900

    [update] 配列の展開代入、配列の展開

--- js/sample.js
+++ js/sample.js
@@ -1,22 +1,25 @@
-const data = {
-    firstName: 'David',
-    lastName: 'Bowie',
-    email: 'starman@gmail.com'
-}
+const arr = [1, 2, 3, 4]
 
-const {
-    firstName,
-    email
-} = data;
-console.log(firstName);
-console.log(email); // -> 'starman@gmail.com'
+const [one, , three] = arr;
 
-const obj = {
-    a: 123,
-    b: 456
-};
+console.log(one) // -> 1
+console.log(three) // -> 3
 
-const {
-    a: num
-} = obj;
-console.log(num) // -> 123
+const users = [
+    '太郎', '花子', '一郎'
+];
+
+const [firstUser] = users;
+console.log(firstUser);
+
+////////////////////////////////
+
+const arrOne = [1, 2, 3, 4];
+const arrTwo = [9, 8, 7];
+
+const arrThree = [
+    ...arrOne,
+    ...arrTwo,
+    "JavaScript"
+];
+console.log(arrThree) // -> [1, 2, 3, 9, 8, 7, "JavaScript"]
