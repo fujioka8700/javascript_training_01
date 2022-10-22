6710dbd5a6f42eac6609efbab92059c2ea06e5d1
fujioka8700 <fujioka8700@gmail.com>
2022-10-22 16:22:12 +0900

    [update] アロー関数

--- js/sample.js
+++ js/sample.js
@@ -1,10 +1,37 @@
-const sum = (a, b) => {
-    return a + b;
+const arr = [1,2,3].map(num => num * 2);
+console.log(arr);
+
+const arr2 = [1,2,3].map(function(num){
+    return num * 2;
+});
+
+console.log(arr2);
+
+setTimeout(() => console.log('ha ha'), 2000);
+
+const func = (a, b) => a + b;
+const n = func(10, 20);
+console.log(n);
+
+const obj = {
+    greeting: name => `Hello, ${name}`
+};
+
+console.log(obj.greeting('太郎'));
+
+///////////////////////////////
+
+const someArray = [1, 2, 3];
+
+function someFunc(item) {
+    console.log(item);
 }
-console.log(sum(10, 15));
 
-const obj = a => a * 10;
-console.log(obj(5))
+var that = this;
+someArray.forEach(function(item) {
+    that.someFunc(item);
+});
 
-const a =()=> console.log('Aです');
-a();
+someArray.forEach(item => {
+    this.someFunc(item);
+});
