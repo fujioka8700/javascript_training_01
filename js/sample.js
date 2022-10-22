3d5b5ea2d3a1f85e4c1ab162befaf25463daab72
fujioka8700 <fujioka8700@gmail.com>
2022-10-22 16:05:24 +0900

    [update] アロー関数

--- js/sample.js
+++ js/sample.js
@@ -1,33 +1,10 @@
-class Human2 {
-    speak() {
-        console.log('こんにちは');
-    }
+const sum = (a, b) => {
+    return a + b;
 }
+console.log(sum(10, 15));
 
-class Girl2 extends Human2 {
-    speak() {
-        console.log('こんにちは！Girlオブジェクト！');
-    }
-}
+const obj = a => a * 10;
+console.log(obj(5))
 
-const obj2 = new Girl2();
-obj2.speak()
-console.log(obj2)
-
-/////////////////////////////////////
-
-var Human = function() {}
-Human.prototype.speak = function() {
-    console.log('こんにちは');
-}
-
-var Girl = function() {}
-Girl.prototype = Object.create(Human.prototype);
-
-Girl.prototype.speak = function() {
-    console.log('こんにちは！Girlオブジェクト！');
-}
-
-const obj = new Girl();
-obj.speak()
-console.log(obj)
+const a =()=> console.log('Aです');
+a();
